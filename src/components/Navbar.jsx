e520959ca0391b2cac942a273a23cc13f1074a23
HardtoPronounce <60298101+HardtoPronounce@users.noreply.github.com>
2023-01-24 19:10:14 +0200

    dasdafs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,26 +3,17 @@ import './styles/navbar.css'
 
 export default function Navbar() {
   return (
-    <>
-    <div>I AM HEREEEEEE</div>
-    <nav className='navigation'>
-        <a href='/' className='brand-name'>
-            Macrosoft
-        </a>
-        <div className='navigation-menu'>
-            <ul>
-                <li>
-                    <a href='/home'>Home</a>
-                </li>
-                <li>
-                    <a href='/about'>About</a>
-                </li>
-                <li>
-                    <a href='/contact'>Contact</a>
-                </li>
-            </ul>
-        </div>
+    <nav classname='nav'>
+        <a href='/' className='site-title'>Companion</a>
+        <ul>
+            <li>
+                <a href='/petCare'>Pet care</a>
+            </li>
+            <li>
+                <a href='/about'>About</a>
+            </li>
+        </ul>
+
     </nav>
-    </>
   )
 }
